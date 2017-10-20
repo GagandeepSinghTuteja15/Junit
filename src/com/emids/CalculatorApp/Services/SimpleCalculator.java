132e3112f8f5414b03fc9440cb7ee47c719b7377
gtuteja <gtuteja@connecture.com>
2017-10-20 18:01:28 +0530

    Third commit

--- src/com/emids/CalculatorApp/Services/SimpleCalculator.java
+++ src/com/emids/CalculatorApp/Services/SimpleCalculator.java
@@ -1,8 +1,11 @@
 package com.emids.CalculatorApp.Services;
 
 public class SimpleCalculator {
-public static void add(String number)
+public static int add(String number)
 {
 String[] ArrayNumeber= number.split(",");	
+if(ArrayNumeber.length==0)
+	return 0;
+return 1;
 }
 }
